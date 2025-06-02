bd873fc3019863b747f08b8c1c068825795e6885
Alex Bucknall <alex.bucknall@gmail.com>
2025-06-02 20:57:07 +0100

    bump

--- flash-your-things/next.config.ts
+++ flash-your-things/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from 'next';
 
+const basePath = process.env.NODE_ENV === 'production' ? (process.env.PAGES_BASE_PATH || '/fyt-website') : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  basePath,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },

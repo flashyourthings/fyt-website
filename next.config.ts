1b2c8b510aadc09eea7a5069db9d795713a77ce6
Alex Bucknall <alex.bucknall@gmail.com>
2025-06-10 21:44:32 +0100

    fixed path

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+// import type { NextConfig } from 'next';
+
+// // const basePath = process.env.NODE_ENV === 'production' ? (process.env.PAGES_BASE_PATH || '/fyt-website') : '';
+// // const basePath = "/"
+
+// const nextConfig: NextConfig = {
+//   // output: 'export',
+//   // basePath,
+//   // assetPrefix: basePath,
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  /* config options here */
+}
+ 
+export default nextConfig
